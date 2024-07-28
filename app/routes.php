b901e1aed318a55ec7f9893fd58be213c7ee10ae
Noziop <contact@fassih.ch>
2024-07-28 16:12:22 +0200

    feat: Update welcome message and add Guzzle dependency
    - Modify app/routes.php to display a message about the upcoming Drawing app
    - Add guzzlehttp/guzzle to composer.json for HTTP requests
    - Update composer.lock to reflect new dependencies

--- app/routes.php
+++ app/routes.php
@@ -6,7 +6,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 return function (App $app) {
     $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello, World!');
+        $response->getBody()->write('Coming soon : Drawing app for Speaker of the Day ritual @Holberton_School Thonon-les-bains!');
         return $response;
     });
 
