2ad97d46a5755e0187047a3bb0665b98b8b93a18
Noziop <contact@fassih.ch>
2024-08-06 18:46:27 +0200

    adding display errors on index, to verbose err500

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+// Set error display based on environment
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
